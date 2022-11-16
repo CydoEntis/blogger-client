5d5f1266862931148be10776b42529ea45570a01
CydoEntis <cydoentis@gmail.com>
2022-11-15 19:56:09 -0500

    Added router and basic routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Outlet,
+} from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Post from "./pages/Post";
+import Write from "./pages/Write";
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/post/:id",
+                element: <Post />,
+            },
+            {
+                path: "/write",
+                element: <Write />,
+            },
+        ],
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/write",
+        element: <Write />,
+    },
+    {
+        path: "/post",
+        element: <Post />,
+    },
+]);
+
 function App() {
-  return (
-    <div>
-      Hello world!
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="container">
+                <RouterProvider router={router} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
