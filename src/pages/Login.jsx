01cae443786c88c63441683f382ae5f1073c0a0a
CydoEntis <cydoentis@gmail.com>
2022-11-19 10:55:05 -0500

    Added register and login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,53 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import axios from "axios";
 
 const Login = () => {
+    const navigate = useNavigate();
+    const [inputs, setInputs] = useState({
+        username: "",
+        password: "",
+    });
+
+    const [err, setErr] = useState(null);
+
+    const handleChange = (e) => {
+        setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post("/auth/login", inputs);
+            navigate("/");
+        } catch (err) {
+            setErr(err.response.data);
+        }
+    };
+
     return (
         <div className="auth">
             <h1>Login</h1>
-            <form action="">
-                <input type="text" placeholder="username" />
-                <input type="password" placeholder="password" />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="username"
+                    name="username"
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                    onChange={handleChange}
+                />
                 <button>Login</button>
-                <p>This is an error!</p>
+                {err && <p>{err}</p>}
                 <span>
                     Don't have an account? <Link to="/register">Register</Link>
                 </span>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,63 @@
+import axios from "axios";
 import React from "react";
-
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
+    const navigate = useNavigate();
+    const [inputs, setInputs] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const [err, setErr] = useState(null);
+
+    const handleChange = (e) => {
+        setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post("/auth/register", inputs);
+            navigate("/login");
+        } catch (err) {
+            setErr(err.response.data);
+        }
+    };
+
     return (
         <div className="auth">
             <h1>Register</h1>
-            <form action="">
-                <input required type="text" placeholder="username" />
-                <input required type="email" placeholder="email" />
-                <input required type="password" placeholder="password" />
-                <button>Login</button>
-                <p>This is an error!</p>
+            <form action="" onSubmit={handleSubmit}>
+                <input
+                    required
+                    type="text"
+                    placeholder="username"
+                    name="username"
+                    onChange={handleChange}
+                />
+                <input
+                    required
+                    type="email"
+                    placeholder="email"
+                    name="email"
+                    onChange={handleChange}
+                />
+                <input
+                    required
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                    onChange={handleChange}
+                />
+                <button>Register</button>
+                {err && <p>{err}</p>}
                 <span>
                     Already have an account? <Link to="/login">Login</Link>
                 </span>
