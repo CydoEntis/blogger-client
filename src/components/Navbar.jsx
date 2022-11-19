847e2034476b128ef32cc84aac958736986006dd
CydoEntis <cydoentis@gmail.com>
2022-11-19 16:16:41 -0500

    Fixed navbar to have login and logout options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
 import Logo from "../img/logo.png";
 
 const Navbar = () => {
+    const { currentUser, logout } = useContext(AuthContext);
+
     return (
         <div className="navbar">
             <div className="container">
@@ -29,8 +32,14 @@ const Navbar = () => {
                     <Link className="link" to="/?cat=food">
                         <h6>Food</h6>
                     </Link>
-                    <span>John</span>
-                    <span>Logout</span>
+                    <span>{currentUser?.username}</span>
+                    {currentUser ? (
+                        <span onClick={logout}>Logout</span>
+                    ) : (
+                        <Link className="link" to="/login">
+                            Login
+                        </Link>
+                    )}
                     <span className="write">
                         <Link className="link" to="/add">
                             Write
