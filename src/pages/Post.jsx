399d59701904dbfd6859aa47a1bfe1db4bae7db5
CydoEntis <cydoentis@gmail.com>
2022-11-29 12:10:44 -0500

    Implement post page styles

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Edit from "../img/edit.png";
-import Delete from "../img/delete.png";
 import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
 import Menu from "../components/Menu";
+
+import { MdEdit } from "react-icons/md";
+import { FaTrash } from "react-icons/fa";
 
 import moment from "moment";
 import { useContext } from "react";
@@ -42,29 +43,50 @@ const Post = () => {
     };
 
     return (
-        <div className="post">
+        <div className="p-3 pt-[80px] md:pt-[100px] md:max-w-[700px] md:mx-auto">
             <div className="content">
                 <img
+                    className="rounded-md"
                     src={`http://localhost:8800/uploads/${post?.img}`}
                     alt=""
                 />
                 <div className="user">
                     {post.userImg && <img src={post.userImg} alt="" />}
-                    <div className="info">
-                        <span>{post.username}</span>
-                        <p>Posted {moment(post.date).fromNow()}</p>
+                    <div className="flex items-center">
+                        <h3 className="text-sm font-fredoka text-med-blue mr-3 capitalize">
+                            {post.username}
+                        </h3>
+                        <p className="font-fredoka text-sm italic text-stone-400">
+                            posted {moment(post.date).fromNow()}
+                        </p>
                     </div>
-                    {currentUser.username === post.username && (
-                        <div className="edit">
-                            <Link to={`/write?edit=${post.id}`} state={post}>
-                                <img src={Edit} alt="" />
-                            </Link>
-                            <img onClick={handleDelete} src={Delete} alt="" />
-                        </div>
-                    )}
                 </div>
-                <h1>{post.title}</h1>
-                <p>{getText(post.desc)}</p>
+                <h1 className="font-fredoka text-med-blue text-2xl capitalize my-2">
+                    {post.title}
+                </h1>
+                <p className="font-fredoka text-med-blue text-md border-b-2 py-3 border-stone-100">
+                    {getText(post.desc)}
+                </p>
+                {currentUser.username === post.username && (
+                    <div className="flex my-3">
+                        <Link
+                            className="bg-vivid-blue hover:bg-cyan-600 py-2 px-1 rounded-md flex justify-center items-center"
+                            to={`/write?edit=${post.id}`}
+                            state={post}
+                        >
+                            <MdEdit className="mx-2 text-white text-xl" />
+                        </Link>
+                        <div
+                            className="mx-2 text-white bg-red-500 text-xl rounded-md hover:bg-red-600 py-2 px-1 rounded-md flex justify-center items-center"
+                            onClick={handleDelete}
+                        >
+                            <FaTrash
+                                className="mx-2 text-white"
+                                onClick={handleDelete}
+                            />
+                        </div>
+                    </div>
+                )}
             </div>
             <Menu cat={post.cat} />
         </div>
