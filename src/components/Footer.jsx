16b286ec0067a92ee2bb41c31b7d489fb1f3eeb7
CydoEntis <cydoentis@gmail.com>
2022-11-30 19:59:39 -0500

    fixed footer to match navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="relative bottom-0 p-5 text-center w-full">
-            <span className=" text-med-blue">
+        <footer className="relative bottom-0 w-full mt-5 bg-white h-[100px]  flex justify-center items-center  p-5 text-center">
+            <span className="text-med-blue">
                 Made with ❤️ and <b className=" text-med-blue">React.js</b>
             </span>
         </footer>
