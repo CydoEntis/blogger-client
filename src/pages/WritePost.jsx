e29ce4c9dcfca0c2c4add16ca9e7a493ceb0d7e0
CydoEntis <cydoentis@gmail.com>
2022-12-02 21:18:17 -0500

    Added select image text display

--- src/pages/WritePost.jsx
+++ src/pages/WritePost.jsx
@@ -12,8 +12,10 @@ const WritePost = () => {
     const navigate = useNavigate();
     const [value, setValue] = useState(state?.desc || "");
     const [title, setTitle] = useState(state?.title || "");
-    const [file, setFile] = useState(null);
+    const [file, setFile] = useState(state?.img || "");
     const [cat, setCat] = useState(state?.cat || "");
+
+    console.log(file);
 
     const upload = async () => {
         try {
@@ -54,18 +56,18 @@ const WritePost = () => {
     };
 
     return (
-        <div className="pt-[80px] lg:pt-[100px] p-3 lg:max-w-[850px] lg:mx-auto xl:max-w-[1000px] 2xl:max-w-[1240px]">
-            <div className="w-full  mb-[100px]">
+        <div className="py-[125px] lg:max-w-[850px] lg:mx-auto xl:max-w-[1000px] 2xl:max-w-[1240px]">
+            <div className="w-full">
                 <input
-                    className="text-med-blue w-full py-1 text-xl xl:text-2xl focus:outline-none"
+                    className="text-med-blue w-full py-1 text-xl xl:text-2xl focus:outline-none my-3 bg-[#f1f1f1] focus:border-b-2 border-med-blue"
                     value={title}
                     type="text"
                     placeholder="Title"
                     onChange={(e) => setTitle(e.target.value)}
                 />
-                <div className="editorContainer">
+                <div className="editorContainer bg-white h-[300px]">
                     <ReactQuill
-                        className="w-full h-[250px]"
+                        className="w-full h-[258px] bg-white"
                         theme="snow"
                         value={value}
                         onChange={setValue}
@@ -74,19 +76,24 @@ const WritePost = () => {
             </div>
             <div className="w-full">
                 <div className="flex justify-between lg:justify-start my-5">
-                    <input
-                        style={{ display: "none" }}
-                        type="file"
-                        name=""
-                        id="file"
-                        onChange={(e) => setFile(e.target.files[0])}
-                    />
-                    <label
-                        className=" text-white bg-med-blue px-3 py-2 rounded-md mb-2 cursor-pointer hover:brightness-75"
-                        htmlFor="file"
-                    >
-                        Upload Image
-                    </label>
+                    <div className="flex justify-center items-center">
+                        <input
+                            style={{ display: "none" }}
+                            type="file"
+                            name=""
+                            id="file"
+                            onChange={(e) => setFile(e.target.files[0])}
+                        />
+                        <label
+                            className=" text-white bg-med-blue px-3 py-2 rounded-md mb-2 cursor-pointer hover:brightness-75"
+                            htmlFor="file"
+                        >
+                            Upload Image
+                        </label>
+                        <p className="mx-3 font-bold text-med-blue">
+                            {(file && file.name) || "Please select an image."}
+                        </p>
+                    </div>
                     <button
                         className=" text-white bg-med-blue px-3 py-2 rounded-md mb-2 lg:mx-3 cursor-pointer hover:brightness-75"
                         onClick={handleClick}
